55fb8a59afa9e974bc71b9264ac433647387e2e3
OhSeChan <seb_s19@naver.com>
2024-05-03 17:04:26 +0900

    feat: 공용 IconButton 컴포넌트 구현

---
+++ src/components/common/IconButton/IconButton.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const IconButtonWrapper = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0;
+	box-sizing: border-box;
+	border: none;
+	background-color: transparent;
+`;
