eb219c8fc3a1abae18e0e90b9467f6da10233890
OhSeChan <seb_s19@naver.com>
2024-05-03 15:26:43 +0900

    feat: iconName 타입 추가 및 반영

--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -7,3 +7,7 @@ export type borderColor = keyof (typeof Theme)['color']['border'];
 export type iconColor = keyof (typeof Theme)['color']['icon'];
 
 export type textColor = keyof (typeof Theme)['color']['text'];
+
+type iconModule = typeof import('@assets/svg/index');
+
+export type iconName = keyof iconModule;
