9ccc528ff80fa6fb3129e3125981851539af9345
OhSeChan <seb_s19@naver.com>
2024-05-05 20:45:05 +0900

    feat: 랜딩페이지 라우팅 추가

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,8 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LandingPage from '@pages/LandingPage/LandingPage';
+import SignInPage from '@pages/SignInPage/SignInPage';
 import { PATH } from '@constants/path';
 import App from './App';
-import SignInPage from './pages/SignInPage/SignInPage';
 
 const AppRouter = () => {
 	const router = createBrowserRouter([
@@ -9,6 +10,10 @@ const AppRouter = () => {
 			path: PATH.ROOT,
 			element: <App />,
 			children: [
+				{
+					path: '',
+					element: <LandingPage />,
+				},
 				{
 					path: PATH.SIGNIN,
 					element: <SignInPage />,
