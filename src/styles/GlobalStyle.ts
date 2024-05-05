3aa0129819374b53697bf719f9e66c84fd5d3679
OhSeChan <seb_s19@naver.com>
2024-05-05 20:43:24 +0900

    feat: page 제작을 위한 global 스타일 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,6 +4,13 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
   body{
     font-family: 'Pretendard', system-ui, -apple-system, BlinkMacSystemFont, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
