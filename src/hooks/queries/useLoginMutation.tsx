0e789dc3c3226ba20307dc06486d444560d8fbe9
0Jaemin0 <jaemin2167@naver.com>
2024-05-10 00:22:49 +0900

    feat: 사용자 정보를 전역 상태 관리에서 query 사용으로 코드 수정

--- src/hooks/queries/useLoginMutation.tsx
+++ src/hooks/queries/useLoginMutation.tsx
@@ -1,12 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import postLogin from '@apis/user/postLogin';
 import { useMutation } from '@tanstack/react-query';
-import useUserStore from '@stores/userStore';
 import { ACCESS_TOKEN } from '@constants/api';
 import { PATH } from '@constants/path';
 
 const useLoginMutation = () => {
-	const { userInfo, setUserInfo } = useUserStore();
 	const navigate = useNavigate();
 
 	const postLoginMutation = useMutation({
@@ -14,10 +12,6 @@ const useLoginMutation = () => {
 		onSuccess: (content) => {
 			localStorage.setItem(ACCESS_TOKEN, content.accessToken);
 			if (content.hasTeam) {
-				setUserInfo({
-					...userInfo!,
-					userId: content.userId,
-				});
 				navigate(PATH.FEED);
 			} else {
 				navigate(PATH.ENTRY);
