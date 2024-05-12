4859037a626525698598da2caa95ab272764135f
0Jaemin0 <jaemin2167@naver.com>
2024-05-13 01:22:40 +0900

    fix: 소셜 로그인 성공에 대한 accessToken 저장 코드 위치 수정

--- src/hooks/queries/useOauthLoginMutation.tsx
+++ src/hooks/queries/useOauthLoginMutation.tsx
@@ -1,23 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import postOauthLogin from '@apis/user/postOauthLogin';
 import { useMutation } from '@tanstack/react-query';
-import useUserStore from '@stores/userStore';
 import { ACCESS_TOKEN } from '@constants/api';
 import { PATH } from '@constants/path';
 
 const useOauthLoginMutation = () => {
-	const { userInfo, setUserInfo } = useUserStore();
 	const navigate = useNavigate();
 
 	const postOauthLoginMutation = useMutation({
 		mutationFn: postOauthLogin,
 		onSuccess: (content) => {
+			localStorage.setItem(ACCESS_TOKEN, content.accessToken);
 			if (content.hasTeam) {
-				localStorage.setItem(ACCESS_TOKEN, content.accessToken);
-				setUserInfo({
-					...userInfo!,
-					userId: content.userId,
-				});
 				navigate(PATH.FEED);
 			} else {
 				navigate(PATH.ENTRY);
