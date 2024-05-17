d77774b5fe5929fbd4b1f7f69c2fdd465ba64b76
0Jaemin0 <jaemin2167@naver.com>
2024-05-17 20:31:58 +0900

    feat: 팀스페이스 가입 처리를 위한 로그인 후 다시 Invite 페이지로 이동하기 위한 로직 추가

--- src/hooks/queries/useOauthLoginMutation.tsx
+++ src/hooks/queries/useOauthLoginMutation.tsx
@@ -1,17 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import postOauthLogin from '@apis/user/postOauthLogin';
 import { useMutation } from '@tanstack/react-query';
-import { ACCESS_TOKEN } from '@constants/api';
+import { ACCESS_TOKEN, INVITE_URL } from '@constants/api';
 import { PATH } from '@constants/path';
 
 const useOauthLoginMutation = () => {
 	const navigate = useNavigate();
+	const inviteUrl = window.sessionStorage.getItem(INVITE_URL);
 
 	const postOauthLoginMutation = useMutation({
 		mutationFn: postOauthLogin,
 		onSuccess: (content) => {
 			localStorage.setItem(ACCESS_TOKEN, content.accessToken);
-			if (content.hasTeam) {
+			if (inviteUrl) {
+				window.sessionStorage.removeItem(INVITE_URL);
+				navigate(`${PATH.INVITE}${inviteUrl}`);
+			} else if (content.hasTeam) {
 				navigate(PATH.FEED);
 			} else {
 				navigate(PATH.ENTRY);
