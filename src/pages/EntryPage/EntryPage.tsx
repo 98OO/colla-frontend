8e7c38bb9e4b46c9db9fb523b4574a36c4b2f8aa
0Jaemin0 <jaemin2167@naver.com>
2024-05-18 18:38:00 +0900

    feat: entryItem 컴포넌트를 사용해 Entry 페이지 구현

--- src/pages/EntryPage/EntryPage.tsx
+++ src/pages/EntryPage/EntryPage.tsx
@@ -1,51 +1,10 @@
-import { useState, ChangeEvent } from 'react';
-import { Button } from '@components/common/Button/Button';
 import Flex from '@components/common/Flex/Flex';
 import Heading from '@components/common/Heading/Heading';
-import Input from '@components/common/Input/Input';
 import Text from '@components/common/Text/Text';
-import useCreateTeamSpaceMutation from '@hooks/queries/useCreateTeamSpaceMutation';
-import useParticipateTeamSpaceMutation from '@hooks/queries/useParticipateTeamSpaceMutation';
-import { AxiosError } from 'axios';
-import { HTTPError } from '@apis/HTTPError';
+import EntryItem from '@components/Entry/EntryItem/EntryItem';
 import { teamCreation, teamParticipation } from '@assets/png';
-import * as S from './EntryPage.styled';
 
 const EntryPage = () => {
-	const [entryData, setEntryData] = useState({
-		teamName: '',
-		teamCode: '',
-	});
-	const [errorText, setErrorText] = useState('');
-	const { mutatePostCreateTeamSpace } = useCreateTeamSpaceMutation();
-	const { mutateParticipateTeamSpace } = useParticipateTeamSpaceMutation(
-		entryData.teamCode
-	);
-
-	const handleChange = (
-		e: ChangeEvent<HTMLInputElement>,
-		fieldName: string
-	) => {
-		const { value } = e.target;
-		setEntryData({
-			...entryData,
-			[fieldName]: value,
-		});
-	};
-
-	const handleCreateTeamSpace = () => {
-		mutatePostCreateTeamSpace(entryData.teamName);
-	};
-
-	const handleParticipateTeamSpace = async () => {
-		try {
-			await mutateParticipateTeamSpace();
-		} catch (error) {
-			const httpError = error as AxiosError<HTTPError>;
-			setErrorText(httpError.message);
-		}
-	};
-
 	return (
 		<Flex direction='column' gap='8' align='center' width='740' padding='10'>
 			<Heading size='lg'>팀스페이스 생성 또는 참가</Heading>
@@ -58,77 +17,22 @@ const EntryPage = () => {
 				paddingTop='20'
 				paddingBottom='20'
 				width='720'>
-				<S.EntryOptionContainer>
-					<Flex direction='column' gap='4'>
-						<Heading size='lg'>생성</Heading>
-						<Text size='md' weight='medium' color='secondary'>
-							팀스페이스를 만들고 협업을 시작하세요
-						</Text>
-					</Flex>
-					<S.ImageWrapper>
-						<img alt='teamCreation' src={teamCreation} />
-					</S.ImageWrapper>
-					<S.InputWrapper>
-						<Text size='md' weight='medium'>
-							팀스페이스 이름
-						</Text>
-						<Input
-							size='lg'
-							placeholder='팀스페이스 이름 입력'
-							isError={false}
-							value={entryData.teamName}
-							maxLength={20}
-							onChange={(e) => handleChange(e, 'teamName')}
-						/>
-					</S.InputWrapper>
-					<Flex direction='column'>
-						<Flex height='28' align='center' />
-						<Button
-							label='생성하기'
-							variant='primary'
-							size='lg'
-							disabled={entryData.teamName.length < 2}
-							onClick={handleCreateTeamSpace}
-						/>
-					</Flex>
-				</S.EntryOptionContainer>
-				<S.EntryOptionContainer>
-					<Flex direction='column' gap='4'>
-						<Heading size='lg'>참가</Heading>
-						<Text size='md' weight='medium' color='secondary'>
-							초대링크를 입력하여 팀스페이스에 참가하세요
-						</Text>
-					</Flex>
-					<S.ImageWrapper>
-						<img alt='teamParticipation' src={teamParticipation} />
-					</S.ImageWrapper>
-					<S.InputWrapper>
-						<Text size='md' weight='medium'>
-							초대링크
-						</Text>
-						<Input
-							size='lg'
-							placeholder='초대링크 입력'
-							isError={false}
-							value={entryData.teamCode}
-							onChange={(e) => handleChange(e, 'teamCode')}
-						/>
-					</S.InputWrapper>
-					<Flex direction='column'>
-						<Flex height='28' align='center'>
-							<Text size='md' weight='regular' color='danger'>
-								{errorText}
-							</Text>
-						</Flex>
-						<Button
-							label='참가하기'
-							variant='primary'
-							size='lg'
-							disabled={entryData.teamCode.length === 0}
-							onClick={handleParticipateTeamSpace}
-						/>
-					</Flex>
-				</S.EntryOptionContainer>
+				<EntryItem
+					type='teamName'
+					title='생성'
+					subTitle='팀스페이스를 만들고 협업을 시작하세요'
+					image={teamCreation}
+					inputLabel='팀스페이스 이름'
+					buttonLabel='생성하기'
+				/>
+				<EntryItem
+					type='teamCode'
+					title='참가'
+					subTitle='초대코드를 입력하여 팀스페이스에 참가하세요'
+					image={teamParticipation}
+					inputLabel='초대코드'
+					buttonLabel='참가하기'
+				/>
 			</Flex>
 		</Flex>
 	);
