a082668f52332850b338ceda20a5fa92f4180f6d
0Jaemin0 <jaemin2167@naver.com>
2024-05-18 18:36:26 +0900

    feat: entry 페이지에서 사용되는 EntryItem 컴포넌트 구현

---
+++ src/components/Entry/EntryItem/EntryItem.styled.ts
@@ -0,0 +1,31 @@
+import { styled } from 'styled-components';
+import theme from '@styles/theme';
+
+export const FormContainer = styled.form`
+	display: flex;
+	flex-direction: column;
+	width: 50%;
+	border: 1px solid ${theme.color.border.primary};
+	padding: ${theme.units.spacing.space16} ${theme.units.spacing.space12};
+	border-radius: ${theme.units.radius.radius6};
+	gap: ${theme.units.spacing.space12};
+`;
+
+export const InputWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: ${theme.units.spacing.space6};
+
+	div {
+		width: 100%;
+	}
+`;
+
+export const ImageWrapper = styled.div`
+	width: 100%;
+
+	img {
+		width: 274px;
+	}
+`;
