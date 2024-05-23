25193a47787c27c4dc7372d1cb938d467c06c8e5
0Jaemin0 <jaemin2167@naver.com>
2024-05-23 12:34:43 +0900

    feat: 공용 SNBIcon 컴포넌트 구현

---
+++ src/components/common/SideNavigationBar/SNBIcon/SNBIcon.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import theme from '@styles/theme';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 88px;
+	height: 100vh;
+
+	padding: ${theme.units.spacing.space24} ${theme.units.spacing.space16};
+	gap: ${theme.units.spacing.space24};
+	border-right: 1px solid ${theme.color.border.tertiary};
+`;
+
+export const ButtonWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	button {
+		padding: ${theme.units.spacing.space8};
+	}
+`;
