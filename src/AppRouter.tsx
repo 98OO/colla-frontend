8f5a969f945c1ee3ef2c5466ecb67bca0e676a24
OhSeChan <seb_s19@naver.com>
2024-05-26 15:42:58 +0900

    feat: 404 에러 처리를 위한 라우팅 추가

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import EntryPage from '@pages/EntryPage/EntryPage';
 import InvitePage from '@pages/InvitePage/InvitePage';
 import LandingPage from '@pages/LandingPage/LandingPage';
+import NotFoundPage from '@pages/NotFoundPage/NotFoundPage';
 import RedirectPage from '@pages/RedirectPage/RedirectPage';
 import SignInPage from '@pages/SignInPage/SignInPage';
 import SignUpPage from '@pages/SignUpPage/SignUpPage';
@@ -13,6 +14,7 @@ const AppRouter = () => {
 		{
 			path: PATH.ROOT,
 			element: <App />,
+			errorElement: <NotFoundPage />,
 			children: [
 				{
 					path: '',
