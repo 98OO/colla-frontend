3b5909dc0d69216440c4722bfc795da8c1684843
OhSeChan <seb_s19@naver.com>
2024-06-05 00:38:56 +0900

    feat: 피드 조회 응답에 따른 피드 타입 작성

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -1,1 +1,71 @@
 export type FeedType = 'NORMAL' | 'COLLECT' | 'VOTE' | 'SCHEDULING';
+
+interface Author {
+	id: number;
+	profileImageUrl: string | null;
+	username: string;
+	tag: {
+		id: number;
+		name: string;
+	} | null;
+}
+
+interface Image {
+	id: number;
+	name: string;
+	fileUrl: string;
+	size: number;
+}
+
+interface Comment {
+	author: Author;
+	content: string;
+	createdAt: string;
+}
+
+interface Attachment {
+	id: number;
+	name: string;
+	fileUrl: string;
+	size: number;
+}
+
+interface NormalDetails {
+	content: string | null;
+}
+
+interface CollectResponse {
+	title: string | null;
+	status: 'PENDING' | 'COMPLETED';
+	updatedAt: string;
+	author: Author;
+}
+
+interface CollectDetails {
+	content: string | null;
+	dueAt: string | null;
+	isClosed: boolean;
+	responses: CollectResponse[];
+}
+
+interface FeedBase {
+	feedId: number;
+	author: Author;
+	title: string;
+	createdAt: string;
+	comments: Comment[];
+	images: Image[];
+	attachments: Attachment[];
+}
+
+interface NormalFeed extends FeedBase {
+	feedType: 'NORMAL';
+	details: NormalDetails;
+}
+
+interface CollectFeed extends FeedBase {
+	feedType: 'COLLECT';
+	details: CollectDetails;
+}
+
+export type FeedData = NormalFeed | CollectFeed;
