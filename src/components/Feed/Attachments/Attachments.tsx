04314819e7f24241ab5008f81a62b59b63275532
OhSeChan <seb_s19@naver.com>
2024-06-05 03:30:56 +0900

    feat: 일반 게시글의 파일을 위한 Attachmnent 컴포넌트 구현

---
+++ src/components/Feed/Attachments/Attachments.tsx
@@ -0,0 +1,48 @@
+import IconButton from '@components/common/IconButton/IconButton';
+import Profile from '@components/common/Profile/Profile';
+import { getUnitFormattedSize } from '@utils/getUnitFormattedSize';
+import * as S from './Attachments.styled';
+
+interface Attachment {
+	id: number;
+	name: string;
+	fileUrl: string;
+	size: number;
+}
+
+interface AttachmentsProps {
+	attachment: Attachment;
+}
+
+const Attachments = ({ attachment }: AttachmentsProps) => {
+	const { name, fileUrl, size } = attachment;
+
+	const filterImageURL = (url: string) => {
+		const imageRegEx = /\.(jpg|jpeg|png|gif|bmp)$/i;
+		return imageRegEx.test(url) ? url : null;
+	};
+
+	return (
+		<S.AttachmentWrapper>
+			<Profile
+				profile={filterImageURL(fileUrl)}
+				initial={name.charAt(0)}
+				avatarSize='lg'
+				avatarShape='rect'
+				title={name}
+				titleSize='lg'
+				titleWeight='medium'
+				text={getUnitFormattedSize(size)}
+			/>
+			<IconButton
+				ariaLabel='download'
+				icon='Download'
+				color='primary'
+				size='md'
+				onClick={() => window.open(fileUrl, '_blank')}
+			/>
+		</S.AttachmentWrapper>
+	);
+};
+
+export default Attachments;
