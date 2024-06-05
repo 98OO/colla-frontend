6b808811bbae40208e1f5de89b20aabc7d396ac2
0Jaemin0 <jaemin2167@naver.com>
2024-06-05 19:07:46 +0900

    fix: 자료저장소 페이지 border 제거

--- src/pages/DocumentPage/DocumentPage.styled.ts
+++ src/pages/DocumentPage/DocumentPage.styled.ts
@@ -4,7 +4,6 @@ import theme from '@styles/theme';
 export const DocumentContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	border: 1px solid black;
 	width: 100%;
 	height: 100%;
 	padding: ${theme.units.spacing.space32};
