d30e5a6abea600963fa0d9ef1b30ee5c2ebcd7bc
0Jaemin0 <jaemin2167@naver.com>
2024-11-07 15:40:45 +0900

    refactor: 채팅방 생성 시 새로운 채팅방 목록을 반영하기 위한 socket 채팅방 목록 초기화

--- src/hooks/queries/chat/useCreateChatChannelMutation.tsx
+++ src/hooks/queries/chat/useCreateChatChannelMutation.tsx
@@ -1,14 +1,17 @@
 import postCreateChatChannel from '@apis/chat/postCreateChatChannel';
 import { useMutation } from '@hooks/queries/common/useMutation';
 import { useQueryClient } from '@tanstack/react-query';
+import useSocketStore from '@stores/socketStore';
 import useToastStore from '@stores/toastStore';
 
 const useCreateChatChannelMutation = () => {
 	const { makeToast } = useToastStore();
 	const queryClient = useQueryClient();
+	const { setChatChannelList } = useSocketStore();
 
 	const handleCreateChatChannelSucess = () => {
 		makeToast('채팅방 생성이 완료됬습니다.', 'Success');
+		setChatChannelList([]);
 		queryClient.invalidateQueries({ queryKey: ['chatChannel'] });
 	};
 
