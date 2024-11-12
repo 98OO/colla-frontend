4c99d2f8560d189cbc9a96ea0cb81af93d41417e
OhSeChan <seb_s19@naver.com>
2024-11-12 22:26:20 +0900

    feat: 에디터 스타일 적용

--- src/components/Post/Editor/Editor.styled.ts
+++ src/components/Post/Editor/Editor.styled.ts
@@ -1,4 +1,5 @@
 import { styled } from 'styled-components';
+import theme from '@styles/theme';
 
 export const EditorContainer = styled.form`
 	display: flex;
@@ -6,4 +7,14 @@ export const EditorContainer = styled.form`
 	width: 680px;
 	min-height: 400px;
 	border-radius: 8px;
+
+	.tiptap {
+		min-height: 400px;
+		max-height: 800px;
+		overflow-y: auto;
+		padding: ${theme.units.spacing.space12};
+		padding-bottom: ${theme.units.spacing.space36};
+		border: 1px solid ${theme.color.border.primary};
+		border-top: none;
+	}
 `;
