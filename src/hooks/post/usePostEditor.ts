cab117ace71c09fc091abac13e7db8f92a1d3435
OhSeChan <seb_s19@naver.com>
2024-11-13 16:12:14 +0900

    feat: 이미지 파일 s3 등록 기능 구현
    - dataTransfer로 File[] 타입의 이미지 파일들을 FileList 타입으로 변환
    - S3에 이미지 파일 등록 및 attachmentUrl 반환 기능 구현

--- src/hooks/post/usePostEditor.ts
+++ src/hooks/post/usePostEditor.ts
@@ -1,7 +1,12 @@
 import { useRef, useState } from 'react';
+import useFileUpload from '@hooks/common/useFileUpload';
+import useUserStatusQuery from '@hooks/queries/useUserStatusQuery';
 import type { Editor } from '@tiptap/react';
 
 const usePostEditor = () => {
+	const { userStatus } = useUserStatusQuery();
+	const { uploadFiles } = useFileUpload();
+
 	const [imageFiles, setImageFiles] = useState<File[]>([]);
 	const editorRef = useRef<Editor>(null);
 
@@ -9,11 +14,36 @@ const usePostEditor = () => {
 		setImageFiles((prevFiles) => [...prevFiles, file]);
 	};
 
+	const getFileList = (files: File[]) => {
+		const dataTransfer = new DataTransfer();
+		files.forEach((file) => dataTransfer.items.add(file));
+
+		return dataTransfer.files;
+	};
+
+	const getAttachmentUrls = async (files: File[], teamspaceId: number) => {
+		const fileList = getFileList(files);
+
+		const attachmentUrls = await uploadFiles(
+			fileList,
+			'TEAMSPACE',
+			teamspaceId
+		);
+
+		return attachmentUrls;
+	};
+
 	const handleSubmit = async () => {
 		if (!editorRef.current) return;
 
+		const teamspaceId = userStatus?.profile.lastSeenTeamspaceId;
+
+		if (!teamspaceId) return;
+
 		const content = editorRef.current.getHTML();
-		console.log(content);
+		const attachmentUrls = await getAttachmentUrls(imageFiles, teamspaceId);
+
+		console.log(content, attachmentUrls);
 	};
 
 	return { editorRef, imageFiles, appendImageFile, handleSubmit };
