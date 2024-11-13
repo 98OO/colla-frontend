c0a4c269e2935f3e5be373353b1b512fc0c31098
OhSeChan <seb_s19@naver.com>
2024-11-13 16:37:07 +0900

    feat: 이미지 url 치환 유틸리티 구현
    - 에디터의 content에 base64로 반영되어 있는 이미지들을 s3에 등록해 받은
      attachmentUrl로 치환하는 기능 구현

--- src/constants/feed.ts
+++ src/constants/feed.ts
@@ -10,3 +10,8 @@ export const EDITOR_IMAGE_ERROR_MESSAGE = {
 	NO_FILE_SELECTED: '파일이 선택되지 않았습니다. 다시 시도해주세요.',
 	EXCEED_LIMIT: '파일 최대 허용 사이즈를 초과했습니다.',
 };
+
+export const REGEX = {
+	DATA_URL: /<img\s+[^>]*src="data:image\/[^"]*"[^>]*>/g,
+	IMG_SRC: /src="[^"]*"/,
+};
--- src/utils/editorImageUtils.ts
+++ src/utils/editorImageUtils.ts
@@ -1,4 +1,24 @@
-export const convertToBase64 = (file: File): Promise<string> => {
+import { REGEX } from '@constants/feed';
+
+const replaceDataUrlsToAttachmentUrls = (
+	content: string,
+	attachmentUrls: string[]
+) => {
+	let imgIndex = 0;
+
+	return content.replace(REGEX.DATA_URL, (match) => {
+		const replacementUrl = attachmentUrls[imgIndex];
+		imgIndex += 1;
+
+		if (replacementUrl) {
+			return match.replace(REGEX.IMG_SRC, `src="${replacementUrl}"`);
+		}
+
+		return match;
+	});
+};
+
+const convertToBase64 = (file: File): Promise<string> => {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader();
 		reader.onload = () => resolve(reader.result as string);
@@ -6,3 +26,5 @@ export const convertToBase64 = (file: File): Promise<string> => {
 		reader.readAsDataURL(file);
 	});
 };
+
+export { replaceDataUrlsToAttachmentUrls, convertToBase64 };
