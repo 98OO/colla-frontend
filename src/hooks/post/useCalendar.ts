be6b4496655f6d289ed4b8ca8d37155de19865d9
OhSeChan <seb_s19@naver.com>
2024-11-18 00:07:48 +0900

    feat: 달력을 위한 날짜 데이터 생성 훅 구현
    - 현재 날짜를 기준으로 이전 달, 현재 달, 다음 달의 날짜를 포함하는
      캘린더 데이터 생성 기능 구현

---
+++ src/hooks/post/useCalendar.ts
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { getDaysInMonth, addMonths, subMonths, startOfDay } from 'date-fns';
+
+const CALENDAR_LENGTH = 35;
+
+interface Day {
+	year: number;
+	month: number;
+	day: number;
+}
+
+const useCalendar = () => {
+	const [curDate, setCurDate] = useState(startOfDay(new Date()));
+
+	const curYear = curDate.getFullYear();
+	const curMonth = curDate.getMonth();
+
+	const prevMonth = subMonths(curDate, 1);
+	const nextMonth = addMonths(curDate, 1);
+
+	const daysInMonth = getDaysInMonth(curDate);
+	const daysInPrevMonth = getDaysInMonth(prevMonth);
+
+	const startDayOfMonth = new Date(curYear, curMonth, 1).getDay();
+
+	const createDayObject = ({ year, month, day }: Day) => ({
+		year,
+		month: month + 1,
+		day,
+	});
+
+	const prevDays = Array.from({ length: startDayOfMonth }).map((_, idx) =>
+		createDayObject({
+			year: prevMonth.getFullYear(),
+			month: prevMonth.getMonth(),
+			day: daysInPrevMonth - startDayOfMonth + idx + 1,
+		})
+	);
+
+	const curDays = Array.from({ length: daysInMonth }).map((_, idx) =>
+		createDayObject({
+			year: curYear,
+			month: curMonth,
+			day: idx + 1,
+		})
+	);
+
+	const remainingDays = CALENDAR_LENGTH - (prevDays.length + curDays.length);
+	const nextDays = Array.from({ length: remainingDays }).map((_, idx) =>
+		createDayObject({
+			year: nextMonth.getFullYear(),
+			month: nextMonth.getMonth(),
+			day: idx + 1,
+		})
+	);
+
+	const calendarDays = [...prevDays, ...curDays, ...nextDays];
+
+	return {
+		curMonth: curMonth + 1,
+		setCurDate,
+		calendarDays,
+	};
+};
+
+export default useCalendar;
