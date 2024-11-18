3f7f4729ecf23caf9245735684428a6d8c63ece1
OhSeChan <seb_s19@naver.com>
2024-11-18 16:45:22 +0900

    feat: 일정 조율 작성 단계 이동을 위한 퍼널 구현

--- src/components/Post/SchedulingPost/SchedulingPost.tsx
+++ src/components/Post/SchedulingPost/SchedulingPost.tsx
@@ -1,15 +1,54 @@
+import { useState } from 'react';
 import { Button } from '@components/common/Button/Button';
 import Flex from '@components/common/Flex/Flex';
 import Calendar from '@components/Post/Calendar/Calendar';
 import * as S from './SchedulingPost.styled';
 
-const SchedulingPost = () => {
+type SchedulingPostStep = 'selectDate' | 'setTime';
+
+interface SelectDateProps {
+	onNext: () => void;
+}
+
+interface SetTimeProps {
+	onPrev: () => void;
+	onSubmit: () => void;
+}
+
+const SelectDate = ({ onNext }: SelectDateProps) => {
 	return (
-		<S.SchedulingPostContainer>
+		<>
 			<Calendar />
 			<Flex justify='flex-end'>
-				<Button label='다음' variant='primary' size='md' onClick={() => {}} />
+				<Button label='다음' variant='primary' size='md' onClick={onNext} />
 			</Flex>
+		</>
+	);
+};
+
+const SetTime = ({ onPrev, onSubmit }: SetTimeProps) => {
+	return (
+		<>
+			<div>시간 설정 단계</div>
+			<Flex justify='flex-end' gap='12'>
+				<Button label='이전' variant='secondary' size='md' onClick={onPrev} />
+				<Button label='등록' variant='primary' size='md' onClick={onSubmit} />
+			</Flex>
+		</>
+	);
+};
+
+const SchedulingPost = () => {
+	const [step, setStep] = useState<SchedulingPostStep>('selectDate');
+
+	return (
+		<S.SchedulingPostContainer>
+			{step === 'selectDate' && (
+				<SelectDate onNext={() => setStep('setTime')} />
+			)}
+			{step === 'setTime' && (
+				<SetTime onPrev={() => setStep('selectDate')} onSubmit={() => {}} />
+			)}
 		</S.SchedulingPostContainer>
 	);
 };
