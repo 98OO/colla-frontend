13ba2d2a7b09f7ec639c9b213b8b77cc92be39d3
ledraco <seb_s25@naver.com>
2026-06-04 19:02:27 +0900

    refactor: 마감 일시를 날짜와 시간으로 분리하여 관리
    - useCalendar, useDaySelection 제거
    - selectedDate, time 상태 직접 관리

--- src/components/Post/SchedulingPost/Section/DueAtSection.tsx
+++ src/components/Post/SchedulingPost/Section/DueAtSection.tsx
@@ -1,29 +1,34 @@
+import { useState } from 'react';
 import Flex from '@components/common/Flex/Flex';
 import Heading from '@components/common/Heading/Heading';
 import Icon from '@components/common/Icon/Icon';
 import Text from '@components/common/Text/Text';
 import DatePicker from '@components/Post/DatePicker/DatePicker';
-import useCalendar from '@hooks/post/useCalendar';
-import useDaySelection from '@hooks/post/useDaySelection';
-import { Day, SchedulingCondition } from '@type/post';
+import type { DateString, SchedulingCondition, Time } from '@type/post';
 import * as S from '../SchedulingPost.styled';
 
 interface DueAtSectionProps {
-	initialDueAt: string;
+	initialDueAtDate: DateString;
+	initialDueAtTime: Time | null;
 	updateCondition: (patch: Partial<SchedulingCondition>) => void;
 }
 
-const DueAtSection = ({ initialDueAt, updateCondition }: DueAtSectionProps) => {
-	const { getInitialDueAt, getFormattedDay } = useCalendar();
-	const initalDueAt = getInitialDueAt(initialDueAt);
-	const { selectedDays, isDaySelected, toggleDaySelection } = useDaySelection(
-		initalDueAt,
-		'single'
-	);
+const DueAtSection = ({
+	initialDueAtDate,
+	initialDueAtTime,
+	updateCondition,
+}: DueAtSectionProps) => {
+	const [selectedDate, setSelectedDate] = useState<DateString>(initialDueAtDate);
+	const [time, setTime] = useState<Time | null>(initialDueAtTime);
 
-	const handleDaySelection = (day: Day) => {
-		toggleDaySelection(day);
-		updateCondition({ dueAt: getFormattedDay(day, true) });
+	const handleDateChange = (newDate: DateString) => {
+		setSelectedDate(newDate);
+		updateCondition({ dueAtDate: newDate });
+	};
+
+	const handleTimeChange = (newTime: Time | null) => {
+		setTime(newTime);
+		updateCondition({ dueAtTime: newTime });
 	};
 
 	return (
@@ -39,9 +44,10 @@ const DueAtSection = ({ initialDueAt, updateCondition }: DueAtSectionProps) => {
 			</Text>
 			<S.DatePickerWrapper>
 				<DatePicker
-					selectedDays={selectedDays}
-					isDaySelected={isDaySelected}
-					toggleDaySelection={handleDaySelection}
+					selectedDate={selectedDate}
+					onDateChange={handleDateChange}
+					time={time}
+					onTimeChange={handleTimeChange}
 				/>
 			</S.DatePickerWrapper>
 		</Flex>
