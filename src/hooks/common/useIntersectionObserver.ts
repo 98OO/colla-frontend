0ae8d043d4acd28a045964c4f2b95ba212fafd5d
0Jaemin0 <jaemin2167@naver.com>
2026-06-20 17:02:43 +0900

    feat: 공통 IntersectionObserver 훅 추가

---
+++ src/hooks/common/useIntersectionObserver.ts
@@ -0,0 +1,29 @@
+import { useInView, type IntersectionOptions } from 'react-intersection-observer';
+
+type UseIntersectionObserverProps = Pick<
+	IntersectionOptions,
+	'root' | 'rootMargin' | 'threshold'
+> & {
+	enabled?: boolean;
+};
+
+const useIntersectionObserver = ({
+	root = null,
+	rootMargin = '0px',
+	threshold = 0,
+	enabled = true,
+}: UseIntersectionObserverProps = {}) => {
+	const { ref, inView } = useInView({
+		root,
+		rootMargin,
+		threshold,
+		skip: !enabled,
+	});
+
+	return {
+		targetRef: ref,
+		isIntersecting: enabled && inView,
+	};
+};
+
+export default useIntersectionObserver;
