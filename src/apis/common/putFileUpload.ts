fa110a337c04500622700ce5557f5fbfd40abc5d
ledraco <seb_s25@naver.com>
2026-07-21 11:24:18 +0900

    fix: presigned 업로드에서 refresh 쿠키의 전송 차단

--- src/apis/common/putFileUpload.ts
+++ src/apis/common/putFileUpload.ts
@@ -6,17 +6,14 @@ export interface FileUploadInfos {
 	contentType: string;
 }
 
-const putFileUpload = async ({
-	presignedURL,
-	file,
-	contentType,
-}: FileUploadInfos) => {
+const putFileUpload = async ({ presignedURL, file, contentType }: FileUploadInfos) => {
 	const response = await axiosInstance.put(presignedURL, file, {
 		headers: {
 			'Content-Type': contentType,
 			'Content-Disposition': `attachment; filename*=UTF-8''${encodeURIComponent(file.name)}`,
 		},
 		authRequired: false,
+		withCredentials: false,
 	});
 
 	return response.data.content;
